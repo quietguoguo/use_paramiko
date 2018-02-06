d5983ac7c530020c372708a481a2df35b93aebeb
guoguo <guoguo@mogujie.com>
2018-02-06 13:55:50 +0800

    线上使用版本

--- remote_withkey.py
+++ remote_withkey.py
@@ -10,17 +10,18 @@ def work(ip,log_file):
     try:
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        pkey='ssh_key'  #本地密钥文件路径
+        pkey='./ssh_key'  #本地密钥文件路径
         key=paramiko.RSAKey.from_private_key_file(pkey)
-        ssh.connect(ip,22,'root',pkey=key,timeout=60)
-#        ssh.connect(ip,22,'root',passwd,timeout=5)
-        stdin, stdout, stderr = ssh.exec_command(" ifconfig e")
+        ssh.connect(ip,22,'root',pkey=key,timeout=30)
+#
+        stdin, stdout, stderr = ssh.exec_command (" ls -l /root)
         for x in stdout.readlines():
             print x.strip("\n")
             log_file.write(ip.strip("\n")+' '+x.strip("\n")+' '+'\n')
+        ssh.close()
     except:
         log_file.write(ip.strip("\n")+' not OK'+'\n')
-    ssh.close()
+        ssh.close()
 try:
     ts=[]
     for i in l.readlines():
@@ -28,9 +29,9 @@ try:
         t=threading.Thread(target=work,args=(i,log_file))
         t.start()
         while True:
-        #判断正在运行的线程数量,如果小于15则退出while循环,目的是限制并发。太多的ssh链接，机器吃不消滴
+        #判断正在运行的线程数量,如果小于5则退出while循环,
         #进入for循环启动新的进程.否则就一直在while循环进入死循环
-            if(len(threading.enumerate()) < 15):
+            if(len(threading.enumerate()) < 60):
                 break
         ts.append(t)
     for t in ts:
