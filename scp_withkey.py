5c3a35afb6f2d3d9f999cd3b5d7c0a8c804bb5c4
guoguo <guoguo@mogujie.com>
2018-02-06 13:50:36 +0800

    线上使用版本

--- scp_withkey.py
+++ scp_withkey.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 import paramiko
 import threading
 import os
@@ -17,25 +18,28 @@ else:
 
 def work(ip,log_file):
     try:
+        print ip
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        pkey='/root/.ssh/id_rsa'  #本地密钥文件路径
-        key=paramiko.RSAKey.from_private_key_file(pkey)
-        ssh.connect(ip,22,'root',pkey=key,timeout=60)
+        pkey='./ssh_key'  #本地密钥文件路径
+        key=paramiko.RSAKey.from_private_key_file(pkey) #有解密密码时,
+        ssh.connect(ip,22,'root',pkey=key,timeout=25)
         sftp = paramiko.SFTPClient.from_transport(ssh.get_transport())
         sftp = ssh.open_sftp()
         sftp.put(local_file,remote_file)
+        sftp.close()
+        ssh.close()
     except Exception as e:
         print e
-    ssh.close()
-    sftp.close()
 try:
     ts=[]
     for i in l.readlines():
         t=threading.Thread(target=work,args=(i,log_file))
         t.start()
         while True:
-            if(len(threading.enumerate()) < 15):
+        #判断正在运行的线程数量,如果小于5则退出while循环,
+        #进入for循环启动新的进程.否则就一直在while循环进入死循环
+            if(len(threading.enumerate()) < 50):
                 break
         ts.append(t)
     for t in ts:
